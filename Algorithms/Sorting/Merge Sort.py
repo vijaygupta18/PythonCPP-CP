b58b607d21441f320c7b01117d511480567d319c
Vijay <66685378+vijaygupta18@users.noreply.github.com>
2020-09-28 21:40:12 +0530

    Create Merge Sort.py

---
+++ Algorithms/Sorting/Merge Sort.py
@@ -0,0 +1,39 @@
+def mergesort(arr):
+    n=len(arr)
+    if n>1:
+        mid=n//2
+        left=arr[:mid]
+        right=arr[mid:]
+        
+        mergesort(left)
+        mergesort(right)
+        
+        i=j=k=0
+        
+        while i<len(left) and j<len(right):
+            if left[i]<right[j]:
+                arr[k]=left[i]
+                i+=1
+            else:
+                arr[k]=right[j]
+                j+=1
+            k+=1
+        
+        #checking for left part
+        while i<len(left):
+            arr[k]=left[i]
+            i+=1
+            k+=1
+        
+        while j<len(right):
+            arr[k]=right[j]
+            j+=1
+            k+=1
+            
+         
+    return arr   
+        
+arr = [14,46,43,27,57,41,45,21,70]
+b=mergesort(arr)  
+
+print(*b)
