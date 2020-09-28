9379a276db964df8e4ba5827d18ffe5c55288466
Vijay <66685378+vijaygupta18@users.noreply.github.com>
2020-09-28 21:38:58 +0530

    Create Insertion Sort.py

---
+++ Algorithms/Sorting/Insertion Sort.py
@@ -0,0 +1,17 @@
+ef IS(a,n):
+    for i in range(1,n):
+        m=i
+        temp=a[m]
+        while(m>0 and temp<a[m-1]):
+            a[m]=a[m-1]
+            m=m-1
+        a[m]=temp   
+                
+    return a
+    
+    
+    
+n=int(input())
+arr=[int(x) for x in input().split()][:n]
+b=IS(arr,n)
+print(*b)
