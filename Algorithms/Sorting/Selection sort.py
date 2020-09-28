92f714e99b1ee08d04b34d1762fda49a8b4d6958
Vijay <66685378+vijaygupta18@users.noreply.github.com>
2020-09-28 21:39:31 +0530

    Create Selection sort.py

---
+++ Algorithms/Sorting/Selection sort.py
@@ -0,0 +1,18 @@
+def ss(a,n):
+    for i in range(n-1):
+        m=i
+        for j in range(i+1,n):
+            if a[j]<a[m]:
+                m=j
+        temp=a[i]
+        a[i]=a[m]
+        a[m]=temp        
+                
+    return a
+    
+    
+    
+n=int(input())
+arr=[int(x) for x in input().split()][:n]
+b=ss(arr,n)
+print(*b)
