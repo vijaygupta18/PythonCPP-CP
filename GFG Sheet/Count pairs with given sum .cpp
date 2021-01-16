cf464a60925eb2c6458cceb53ea91b82d102d0aa
Vijay <66685378+vijaygupta18@users.noreply.github.com>
2021-01-16 17:56:26 +0530

    Create Count pairs with given sum .cpp

---
+++ GFG Sheet/Count pairs with given sum .cpp
@@ -0,0 +1,49 @@
+//https://practice.geeksforgeeks.org/problems/count-pairs-with-given-sum5022/1
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+//User function template for C++
+
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        // code here
+        map<int,int>m;
+        for(int i=0;i<n;i++)
+        m[arr[i]]+=1;
+        int count=0;
+        for(int i=0;i<n;i++){
+            count+=m[k-arr[i]];
+            if(k-arr[i]==arr[i])
+            count--;
+        }
+        return count/2;
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getPairsCount(arr, n, k);
+        cout << ans << "\n";
+    }
+    
+    return 0;
+}  // } Driver Code Ends
